e0050d18b2b1a06ae93554f89d991cf91ffd28bf
bexi <trreitmaier@msn.com>
2018-09-16 23:34:46 +0200

    inital semaphore for critical areas

---
+++ src/CriticalArea.java
@@ -0,0 +1,26 @@
+
+enum CriticalArea {
+	
+	// id from top
+	
+    // Top Station (top railway) // bottom part?
+    // Crossroad
+    // Right part
+    // Center part (top railway) // bottom part? 
+    // Left part
+    // Bottom Station (bottom railway) // top part? 
+	
+	TopStation(0),
+	Crossroad(1),
+	Right(2),
+	Center(3),
+	Left(4),
+	BottomStation(5);
+	
+	public final int id;  
+	
+    CriticalArea(int id) {
+        this.id = id;
+    }
+
+}
