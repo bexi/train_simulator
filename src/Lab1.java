ab184485d9d619e3ec46bdc5811ef575f2f6a792
bexi <trreitmaier@msn.com>
2018-09-17 13:29:44 +0200

    added some comments for structure and semaphores

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,19 @@
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+
 import TSim.*;
 
 public class Lab1 {
+	
+	// Semaphores for the critical areas, e.i. areas where there can only be one train at a time 
+	
+	Map<CriticalArea, Semaphore> semaphores = new EnumMap<CriticalArea, Semaphore>(CriticalArea.class);
+	//enumMap.put(Color.RED, "red");
+	//String value = enumMap.get(Color.RED);
+	
+	
+	// Sensors  
 
   public Lab1(int speed1, int speed2) {
 //    TSimInterface tsi = TSimInterface.getInstance();
@@ -12,6 +25,18 @@ public class Lab1 {
 //      e.printStackTrace();    // or only e.getMessage() for the error
 //      System.exit(1);
 //    }
+	  System.out.println(CriticalArea.TopStation.id);
+	  
+	  // Create semaphore for all critical areas
+	  // Semaphore semaphore = new Semaphore(1);
+	  
+	  
+	  for( CriticalArea area : CriticalArea.values()) {
+		  System.out.println(area);
+		  semaphores.put(area, new Semaphore(1));
+	  }
+	  //System.out.println("length of semaphore map: " + semaphores.size()); --> 6 
+
 	  Train train1 = new Train(1, speed1);
 	  train1.run();
 	  Train train2 = new Train(2, speed2);
@@ -19,3 +44,6 @@ public class Lab1 {
 
   }
 }
+
+
+// question: using streams within a thread ? 
--- src/TSim/Train.java
+++ src/TSim/Train.java
@@ -5,6 +5,7 @@ public class Train extends Thread {
 
 	private int train_id;
 	private int train_speed;
+	private boolean down;
 	
 	public Train(int train_id, int train_speed) {
 		tsi = TSimInterface.getInstance();
@@ -16,11 +17,35 @@ public class Train extends Thread {
 	public void run() {
 	    try {
 	        tsi.setSpeed(train_id, train_speed);
+	        
+	        // set up for the two trains 
+	        if(this.train_id==1) {
+	        	// train 1 starts at the topStation
+	        	this.down=true;
+	        	// get semaphore for topStation
+	        }
+	        if(this.train_id==2) {
+	        	// train 2 starts at the bottomStation
+	        	this.down=false;
+	        	// get semaphore for bottomStation
+	        }
+	        
+	        // fetch all sensor events
+	        while(true) {
+	        	// sensor event 
+	            SensorEvent event = tsi.getSensor(train_id);
+	            System.out.println("Event: " + event);
+	            System.out.println("Event pos: " + event.xPos + " " + event.yPos);
+
+	        }
 	      }
 	      catch (CommandException e) {
 	        e.printStackTrace();    // or only e.getMessage() for the error
 	        System.exit(1);
-	      }
+	      } catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
