ef6094c360b95ce07c51d06392c5ecf23646a545
bexi <trreitmaier@msn.com>
2018-09-17 17:47:30 +0200

    trains is almost simul, needs to fix stations and placement of sensors

--- src/Lab1.java
+++ src/Lab1.java
@@ -94,7 +94,10 @@ public class Lab1 {
 		        // handle all sensor events
 		        while (!this.isInterrupted()) {
 		            SensorEvent event = tsi.getSensor(train_id);
-		            this.handleSensorEvent(event);
+					// ignore events which is outgoing from sensors
+					if(event.getStatus() == SensorEvent.ACTIVE) {
+			            this.handleSensorEvent(event);
+					}
 		          }
 		      }
 		      catch (CommandException e) {
@@ -106,50 +109,156 @@ public class Lab1 {
 			} 
 		}
 
-		private void handleSensorEvent(SensorEvent event) throws CommandException {
+		private void handleSensorEvent(SensorEvent event) throws CommandException, InterruptedException {
 			// TODO Auto-generated method stub
 			System.out.println(event);
 			
-			// Sensor X (after crossroad top)
-//			if(event.getXpos()== 14 & event.getYpos()==7) {
-//				
-//				// set speed to 0 - backup if the semaphore blocks 
-//				tsi.setSpeed(train_id, train_slowdown);
-//				// try to acquire semaphore
-//				semaphores.get(CriticalArea.Right);
-//				semaphores.get(CriticalArea.TopStation).release();
-//				// set speed
-//				tsi.setSpeed(train_id, train_speed);
-//				// set switch to UP
-//				tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-//			}
 			
-			if(this.isEqualSensor()) {
-				// going down, next semaphor: Left
+			// cross section - north sensors (in and out of cross section)
+			if(
+					this.isEqualSensor(event, Sensor.NorthTop) || 
+					this.isEqualSensor(event, Sensor.NorthBottom)) 
+			{
 				if(this.down) {
-					this.tryAcquire();
-					semaphores.get(CriticalArea.TopStation).release();
-					// set switch to UP
-					tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-					tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);	
+					// going into cross section
+					this.tryAcquire(CriticalArea.Crossroad);
+					semaphores.get(CriticalArea.TopStation).release();	
 				}else {
-					// going up, next: north station 
+					// has been in cross section -> release semaphor
+					semaphores.get(CriticalArea.Crossroad).release();
+					this.enterStation();
 				}
-
 			}
 			
 			
+			// cross section - south sensors (in and out of cross section)
+			if(
+					this.isEqualSensor(event, Sensor.NorthCenterTop1) || 
+					this.isEqualSensor(event, Sensor.NorthCenterBottom1)) 
+			{
+				if(this.down) {
+					// has been in cross section --> release semaphor
+					semaphores.get(CriticalArea.Crossroad).release();
+					// do not let go of north top station semaphor yet 
+				}else {
+					// going into cross section
+					this.tryAcquire(CriticalArea.Crossroad);
+				}
+			}
+			
+			// going into east section from the north
+			if(this.isEqualSensor(event, Sensor.NorthCenterTop2) && this.down) {
+				this.tryAcquire(CriticalArea.Right);
+				semaphores.get(CriticalArea.TopStation).release();
+				// set switch to UP
+				tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
+			}
+			if(this.isEqualSensor(event, Sensor.NorthCenterBottom2) && this.down) {
+				this.tryAcquire(CriticalArea.Right);
+				// release possible semaphor for the other north station? 
+				tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);	
+			}	
+			
+			// going into east section from the south
+			if(this.isEqualSensor(event, Sensor.SouthCenterTop) && !this.down) {
+				this.tryAcquire(CriticalArea.Right);
+				semaphores.get(CriticalArea.Center).release();
+				tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+			}
+			if(this.isEqualSensor(event, Sensor.SouthCenterBottom) && !this.down) {
+				this.tryAcquire(CriticalArea.Right);
+				// release possible semaphor for the other north station? 
+				tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);	
+			}
+			
+			// going out of east section (either to the north or south)
+			if(this.isEqualSensor(event, Sensor.East)) {
+				// going into the Center section
+				if(this.down) {
+					// try acquire the Center sem
+					if(semaphores.get(CriticalArea.Center).tryAcquire()) {
+						tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+					}
+					// otherwise take the other route 
+					else {
+						tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);
+					}
+				}
+				// going into the NorthCenter section
+				else {
+					// try acquire the TopStation sem
+					if(semaphores.get(CriticalArea.TopStation).tryAcquire()) {
+						tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+					}
+					// otherwise take the other route 
+					else {
+						tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);
+					}
+					
+				}
+				// release semaphore when leaving the area
+				semaphores.get(CriticalArea.Right).release();
+			}
+			
+			// going into center section from the north - done in previous scope 
+			
+			// going into the center from the south 			
+			if(this.isEqualSensor(event, Sensor.West) && !this.down) {
+				// try acquire the TopStation sem
+				if(semaphores.get(CriticalArea.Center).tryAcquire()) {
+					tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
+				}
+				// otherwise take the other route (longer)
+				else {
+					tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
+				}
+				semaphores.get(CriticalArea.Left).release();
+			}
+			
+			// going into west section from north
+			if(this.isEqualSensor(event, Sensor.SouthCenterTop) && this.down) {
+				this.tryAcquire(CriticalArea.Left);
+				semaphores.get(CriticalArea.Center).release();
+				// set switch to UP
+				tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
+			}
+			if(this.isEqualSensor(event, Sensor.SouthCenterBottom) && this.down) {
+				this.tryAcquire(CriticalArea.Left);
+				// release possible semaphor for the other north station? 
+				tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);	
+			}	
+			// going into west section from south
+			if(this.isEqualSensor(event, Sensor.SouthTop) && !this.down) {
+				this.tryAcquire(CriticalArea.Left);
+				// release possible sem? 
+				// set switch to UP
+				tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
+			}
+			if(this.isEqualSensor(event, Sensor.SouthBottom) && !this.down) {
+				this.tryAcquire(CriticalArea.Left);
+				semaphores.get(CriticalArea.BottomStation).release();
+				tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);	
+			}	
+			
+		}
+
+		private void enterStation() throws CommandException {
+			//slow down
+			tsi.setSpeed(train_id, 0);
+			// pause at station
+			
+			// start in the other direction 			
 		}
 
 		private boolean isEqualSensor(SensorEvent event, Sensor sensor) {
 			return event.getXpos() == sensor.x && event.getYpos() == sensor.y;
 		}
 
-		private void tryAcquire() throws CommandException {
+		private void tryAcquire(CriticalArea ca) throws CommandException, InterruptedException {
 			// set speed to 0 - backup if the semaphore blocks 
 			tsi.setSpeed(train_id, train_slowdown);
 			// try to acquire semaphore
-			semaphores.get(CriticalArea.Right);
+			semaphores.get(ca).acquire();
 			// set speed
 			tsi.setSpeed(train_id, train_speed);			
 		}
