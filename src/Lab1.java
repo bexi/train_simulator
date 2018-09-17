cca828aac9b78af9909267d631166e54b89c57cc
bexi <trreitmaier@msn.com>
2018-09-17 14:42:11 +0200

    moved the train class into the lab1 class since they need to share the semaphores

--- src/Lab1.java
+++ src/Lab1.java
@@ -8,7 +8,7 @@ public class Lab1 {
 	
 	// Semaphores for the critical areas, e.i. areas where there can only be one train at a time 
 	
-	Map<CriticalArea, Semaphore> semaphores = new EnumMap<CriticalArea, Semaphore>(CriticalArea.class);
+	private Map<CriticalArea, Semaphore> semaphores = new EnumMap<CriticalArea, Semaphore>(CriticalArea.class);
 	//enumMap.put(Color.RED, "red");
 	//String value = enumMap.get(Color.RED);
 	
@@ -43,7 +43,69 @@ public class Lab1 {
 	  train2.start();
 
   }
+  
+  // Internal class for a train
+  public class Train extends Thread {
+		private TSimInterface tsi;
+
+		private int train_id;
+		private int train_speed;
+		private boolean down;
+		
+		
+		public Train(int train_id, int train_speed) {
+			tsi = TSimInterface.getInstance();
+			this.train_id = train_id;
+			this.train_speed = train_speed;
+			
+		}
+		
+		public void run() {
+		    try {
+		        tsi.setSpeed(train_id, train_speed);
+		        
+		        // set up for the two trains 
+		        if(this.train_id==1) {
+		        	// train 1 starts at the topStation
+		        	this.down=true;
+		        	// get semaphore for topStation
+		        	
+		        	
+		        }
+		        if(this.train_id==2) {
+		        	// train 2 starts at the bottomStation
+		        	this.down=false;
+		        	// get semaphore for bottomStation
+		        }
+		        
+		        // handle all sensor events
+		        while (!this.isInterrupted()) {
+		            SensorEvent event = tsi.getSensor(train_id);
+		            this.handleSensorEvent(event);
+		          }
+		      }
+		      catch (CommandException e) {
+		        e.printStackTrace();    // or only e.getMessage() for the error
+		        System.exit(1);
+		      } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		}
+
+		private void handleSensorEvent(SensorEvent event) {
+			// TODO Auto-generated method stub
+			System.out.println(event);
+			
+			
+		}
+
+	}
+
 }
+
+
+
 
 
 // question: using streams within a thread ? 
