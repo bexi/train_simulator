5cfedee6e39c8f945797396b8f8e495de3a72725
bexi <trreitmaier@msn.com>
2018-09-18 10:46:49 +0200

    fixed the sensors for the center part

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,7 +1,8 @@
 
 public enum Sensor {
-	
+	NorthTopStation(14,3),
 	NorthTop(6,6),
+	NorthBottomStation(14,5),
 	NorthBottom(9,5),
 	NorthCenterTop1(10,7),
 	NorthCenterTop2(14,7),
@@ -9,12 +10,14 @@ public enum Sensor {
 	NorthCenterBottom2(16,8),
 	East(19,8),
 	SouthCenterTop2(14,9),
-	SouthCenterTop1(8,9),
+	SouthCenterTop1(6,9),
 	SouthCenterBottom2(14,10),
-	SouthCenterBottom1(8,10),
+	SouthCenterBottom1(6,10),
 	West(1,10),
 	SouthTop(5,11),
-	SouthBottom(5,13);
+	SouthTopStation(14,11),
+	SouthBottom(5,13),
+	SouthBottomStation(14,13);
 	
 	public final int x;
 	public final int y;
