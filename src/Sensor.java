1429ed7be8730ebccae2a3063534e899a0e80d7d
bexi <trreitmaier@msn.com>
2018-09-19 09:17:41 +0200

    moved some sensors away from switch

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,13 +1,13 @@
 
 public enum Sensor {
-	NorthTopStation(14,3),
+	NorthTopStation(16,3),
 	NorthTop(6,6),
-	NorthBottomStation(14,5),
+	NorthBottomStation(16,5),
 	NorthBottom(9,5),
 	NorthCenterTop1(10,7),
 	NorthCenterTop2(14,7),
 	NorthCenterBottom1(10,8),
-	NorthCenterBottom2(16,8),
+	NorthCenterBottom2(15,8),
 	East(19,8),
 	SouthCenterTop2(12,9),
 	SouthCenterTop1(7,9),
@@ -15,9 +15,9 @@ public enum Sensor {
 	SouthCenterBottom1(7,10),
 	West(1,10),
 	SouthTop(5,11),
-	SouthTopStation(14,11),
+	SouthTopStation(16,11),
 	SouthBottom(5,13),
-	SouthBottomStation(14,13);
+	SouthBottomStation(16,13);
 	
 	public final int x;
 	public final int y;
