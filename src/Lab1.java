67c67eaf7030ce6c91ff8e87d2303c6675e462c4
bexi <trreitmaier@msn.com>
2018-09-19 09:41:23 +0200

    clean up code

--- src/Lab1.java
+++ src/Lab1.java
@@ -7,59 +7,30 @@ import TSim.*;
 public class Lab1 {
 	
 	// Semaphores for the critical areas, e.i. areas where there can only be one train at a time 
-	
 	public Map<CriticalArea, Semaphore> semaphores = new EnumMap<CriticalArea, Semaphore>(CriticalArea.class);
-	//enumMap.put(Color.RED, "red");
-	//String value = enumMap.get(Color.RED);
 	
-	
-	// Sensors  
-	public int[] sensors;
-
-  public Lab1(int speed1, int speed2) {
-//    TSimInterface tsi = TSimInterface.getInstance();
-//
-//    try {
-//      tsi.setSpeed(1,speed1);
-//    }
-//    catch (CommandException e) {
-//      e.printStackTrace();    // or only e.getMessage() for the error
-//      System.exit(1);
-//    }
-	  
-	  
-	  // Create semaphore for all critical areas
-	  // Semaphore semaphore = new Semaphore(1);
-	  
-	  
-	  for( CriticalArea area : CriticalArea.values()) {
-		  semaphores.put(area, new Semaphore(1));
-	  }
-	  
-	  this.addSensors();
-	  //System.out.println("length of semaphore map: " + semaphores.size()); --> 6 
-
-	  Train train1 = new Train(1, speed1);
-	  train1.start();
-	  Train train2 = new Train(2, speed2);
-	  train2.start();
-
-  }
-  
-  private void addSensors() {
-		// TODO Auto-generated method stub
-	  
-		
+	public Lab1(int speed1, int speed2) {
+		// Create the actual semaphores for the CriticalArea enums 
+		for( CriticalArea area : CriticalArea.values()) {
+			semaphores.put(area, new Semaphore(1));
+		}
+		// Create the two trains for the simulation
+		Train train1 = new Train(1, speed1);
+		train1.start();
+		Train train2 = new Train(2, speed2);
+		train2.start();
 	}
 
-// Internal class for a train
-  public class Train extends Thread {
+	// Internal class for a train
+	public class Train extends Thread {
+		// interface for communication with the tsim
 		private TSimInterface tsi;
 
 		private int train_id;
 		private int train_speed;
 		private boolean down;
 		private int train_slowdown = 0;
+		
 		// states for who got the semaphore where there is two possible paths
 		private boolean gotNorth;
 		private boolean gotSouth;
@@ -76,23 +47,20 @@ public class Lab1 {
 		    try {
 		        tsi.setSpeed(train_id, train_speed);
 		        
-		        // set up for the two trains 
+		        // setup for the two trains 
 		        if(this.train_id==1) {
 		        	// train 1 starts at the topStation
 		        	this.down=true;
 		        	// get semaphore for topStation
 		        	semaphores.get(CriticalArea.NorthStation).acquire();
 		        	this.gotNorth = true;
-		        	
-		        	
-		        }
+		        }        
 		        if(this.train_id==2) {
 		        	// train 2 starts at the bottomStation
 		        	this.down=false;
 		        	// get semaphore for bottomStation
 		        	semaphores.get(CriticalArea.SouthStation).acquire();
 		        	this.gotSouth = true;
-
 		        }
 		        
 		        // handle all sensor events
@@ -105,70 +73,59 @@ public class Lab1 {
 		          }
 		      }
 		      catch (CommandException e) {
-		        e.printStackTrace();    // or only e.getMessage() for the error
+		        e.printStackTrace();    
 		        System.exit(1);
 		      } catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} 
 		}
 
 		private void handleSensorEvent(SensorEvent event) throws CommandException, InterruptedException {
-			//System.out.println(event);
 			
 			// --------------- NORTH SECTION (from north)
-
 			// north station
 			if((this.isEqualSensor(event, Sensor.NorthTopStation) || this.isEqualSensor(event, Sensor.NorthBottomStation) ) && !this.down) {
 				this.enterStation();
-			}
-						
+			}				
 			// North -> Crossroad
 			if((this.isEqualSensor(event, Sensor.NorthTop) || this.isEqualSensor(event, Sensor.NorthBottom) ) && this.down) {
-					this.tryAcquire(CriticalArea.Crossroad);
-
+					this.acquire(CriticalArea.Crossroad);
 			}
 			// (Crossroad) -> North
 			if((this.isEqualSensor(event, Sensor.NorthTop) || this.isEqualSensor(event, Sensor.NorthBottom) ) && !this.down) {
 				this.release(CriticalArea.Crossroad);
 			}
-
 			// (Crossroad) -> NorthCenter
 			if((this.isEqualSensor(event, Sensor.NorthCenterTop1) || this.isEqualSensor(event, Sensor.NorthCenterBottom1) ) && this.down) {
 				this.release(CriticalArea.Crossroad);	
 			}
 			// NorthCenter -> Crossroad
 			if((this.isEqualSensor(event, Sensor.NorthCenterTop1) || this.isEqualSensor(event, Sensor.NorthCenterBottom1) ) && !this.down) {
-				this.tryAcquire(CriticalArea.Crossroad);
-			}
-			
+				this.acquire(CriticalArea.Crossroad);
+			}		
 			// NorthCenterTop -> East 
 			if(this.isEqualSensor(event, Sensor.NorthCenterTop2)  && this.down) {
-				this.tryAcquire(CriticalArea.East);	
+				this.acquire(CriticalArea.East);	
 				tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);	
 			}
 			// NorthCenterBottom -> East 
 			if(this.isEqualSensor(event, Sensor.NorthCenterBottom2) && this.down) {
-				this.tryAcquire(CriticalArea.East);	
+				this.acquire(CriticalArea.East);	
 				tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);	
-			}
-			
+			}	
 			// (NorthStation / NorthCenterTop) -> East   
 			if(this.isEqualSensor(event, Sensor.East) && this.down) {
 				if(this.gotNorth) {
 					this.release(CriticalArea.NorthStation);
 					this.gotNorth = false;
 				}
-			}
-			
+			}	
 			
 			// --------------- EAST SECTION (from east)
-
 			// (East) -> NorthCenter
 			if((this.isEqualSensor(event, Sensor.NorthCenterTop2) || this.isEqualSensor(event, Sensor.NorthCenterBottom2)) && !this.down) {
 				this.release(CriticalArea.East);
 			}
-			
 			// East -> NorthCenter
 			if(this.isEqualSensor(event, Sensor.East) && !this.down) {
 				// two possible roads
@@ -182,7 +139,6 @@ public class Lab1 {
 					tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
 				}				
 			}	
-			
 			// East -> SouthCenter
 			if(this.isEqualSensor(event, Sensor.East) && this.down) {
 				System.out.println("EAST SENSOR");
@@ -205,26 +161,24 @@ public class Lab1 {
 			// --------------- CENTER SECTION (from center)
 			// SouthCenterTop -> East
 			if(this.isEqualSensor(event, Sensor.SouthCenterTop2) && !this.down) {
-				this.tryAcquire(CriticalArea.East);
+				this.acquire(CriticalArea.East);
 				tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
 			}
 			// SouthCenterBottom -> East
 			if(this.isEqualSensor(event, Sensor.SouthCenterBottom2) && !this.down) {
-				this.tryAcquire(CriticalArea.East);
+				this.acquire(CriticalArea.East);
 				tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);
 			}
-			
 			// SouthCenterTop -> West
 			if(this.isEqualSensor(event, Sensor.SouthCenterTop1) && this.down) {
-				this.tryAcquire(CriticalArea.West);
+				this.acquire(CriticalArea.West);
 				tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
 			}
 			// SouthCenterBottom -> West
 			if(this.isEqualSensor(event, Sensor.SouthCenterBottom1) && this.down) {
-				this.tryAcquire(CriticalArea.West);
+				this.acquire(CriticalArea.West);
 				tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
-			}
-			
+			}			
 			// (Center) -> West
 			if(this.isEqualSensor(event, Sensor.West) && this.down) {
 				if(this.gotCenter) {
@@ -249,8 +203,7 @@ public class Lab1 {
 			// (West) -> SouthStation
 			if((this.isEqualSensor(event, Sensor.SouthTop) || this.isEqualSensor(event, Sensor.SouthBottom))&& this.down) {
 				this.release(CriticalArea.West);
-			}
-			
+			}	
 			// West -> SouthCenter
 			if(this.isEqualSensor(event, Sensor.West) && !this.down) {	
 	            if (semaphores.get(CriticalArea.Center).tryAcquire()) {
@@ -261,7 +214,6 @@ public class Lab1 {
 	              tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
 	            }
 			}
-
 			// West -> SouthStation
 			if(this.isEqualSensor(event, Sensor.West) && this.down) {
 				// two possible roads
@@ -274,16 +226,16 @@ public class Lab1 {
 					tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
 				}		
 			}
-
+			
 			// --------------- SOUTH SECTION (from SouthStations)
 			// SouthStationTop -> West
 			if(this.isEqualSensor(event, Sensor.SouthTop) && !this.down) {
-				this.tryAcquire(CriticalArea.West);
+				this.acquire(CriticalArea.West);
 				tsi.setSwitch(3, 11, TSimInterface.SWITCH_LEFT);
 			}
 			// SouthStationBottom -> West
 			if(this.isEqualSensor(event, Sensor.SouthBottom) && !this.down) {
-				this.tryAcquire(CriticalArea.West);
+				this.acquire(CriticalArea.West);
 				tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
 			}
 			// (SouthStation Top) -> West
@@ -293,37 +245,34 @@ public class Lab1 {
 					this.gotSouth = false;
 				}
 			}
-			
 			// SouthStation
 			if((this.isEqualSensor(event, Sensor.SouthTopStation) || this.isEqualSensor(event, Sensor.SouthBottomStation) ) && this.down) {
 				this.enterStation();
 			}
-			
 		}
 
 		private void release(CriticalArea ca) {
 			semaphores.get(ca).release();
-			System.out.println("release: " + ca+ " train_id: " + this.train_id);	
+			//System.out.println("release: " + ca + " train_id: " + this.train_id);	
 		}
 
 		private void enterStation() throws CommandException, InterruptedException {
-			//slow down
+			// slow down
 			tsi.setSpeed(train_id, 0);
 			// pause at station
 			Thread.sleep(2000);
 			// change direction 	
 		    this.train_speed = -this.train_speed;
 		    this.down = !this.down;
+		    // accelerate train again
 			tsi.setSpeed(train_id, train_speed);
-
-		    System.out.println("at station!");
 		}
 
 		private boolean isEqualSensor(SensorEvent event, Sensor sensor) {
 			return event.getXpos() == sensor.x && event.getYpos() == sensor.y;
 		}
 
-		private void tryAcquire(CriticalArea ca) throws CommandException, InterruptedException {
+		private void acquire(CriticalArea ca) throws CommandException, InterruptedException {
 			// set speed to 0 - backup if the semaphore blocks 
 			tsi.setSpeed(train_id, train_slowdown);
 			// try to acquire semaphore
@@ -331,11 +280,6 @@ public class Lab1 {
 			System.out.println("acquire: " + ca + " train_id: " + this.train_id);
 			// set speed
 			tsi.setSpeed(train_id, train_speed);	
-			// set the correct state
-//			this.acquiredNewSemaphore = true;
-//			this.selectedCriticalArea = CriticalArea.Crossroad;
 		}
-
 	}
-
 }
